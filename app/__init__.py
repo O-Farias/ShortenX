831c01680ec0854af10befe97f003fb7696b4ae7
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-10 19:53:47 +0000

    adiciona tela do `frontend`

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ migrate = Migrate()
 
 def create_app():
     load_dotenv()
-    app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///shortify.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,27 @@
-from flask import Blueprint, request, jsonify, redirect
+from flask import Blueprint, render_template, request, redirect
 from app.models import URL
 from app.database import db
 
-shortener_bp = Blueprint('shortener', __name__)
+shortener_bp = Blueprint('shortener', __name__, template_folder="../templates")
+
+@shortener_bp.route('/')
+def home():
+    return render_template('home.html')
 
 @shortener_bp.route('/shorten', methods=['POST'])
 def shorten_url():
-    data = request.json
-    original_url = data.get('original_url')
+    original_url = request.form.get('original_url')
 
     if not original_url:
-        return jsonify({'error': 'URL inválida'}), 400
+        return render_template('home.html', error="URL inválida. Tente novamente.")
 
     short_url = URL.generate_short_url()
     new_url = URL(original_url=original_url, short_url=short_url)
-    db.session.add(new_url)
-    db.session.commit()
 
-    return jsonify({'original_url': original_url, 'short_url': short_url})
+    with db.session.begin():
+        db.session.add(new_url)
+
+    return render_template('home.html', short_url=short_url, original_url=original_url)
 
 @shortener_bp.route('/<short_url>')
 def redirect_to_url(short_url):
@@ -26,4 +30,4 @@ def redirect_to_url(short_url):
     if url:
         return redirect(url.original_url)
     else:
-        return jsonify({'error': 'URL não encontrada'}), 404
+        return render_template('home.html', error="URL não encontrada.")
